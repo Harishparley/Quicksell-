63c9dee1686dbfcf7f23582e0169094113140b45
Harishparley <harishparleyhp@gmail.com>
2025-12-22 03:18:41 +0530

    new model created for chat

---
+++ models/message.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiver: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  isRead: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
