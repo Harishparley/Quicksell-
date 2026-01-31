4f600a200d95a74735c871291172322e186981b7
Harishparley <harishparleyhp@gmail.com>
2026-01-31 19:38:53 +0530

    update the code

--- controllers/review.js
+++ controllers/review.js
@@ -1,8 +1,7 @@
 const Review = require("../models/review.js");
-const Product = require("../models/product.js"); // CHANGED: Import Product
+const Product = require("../models/product.js"); 
 
 module.exports.createReview = async (req, res) => {
-  // CHANGED: Find Product instead of Listing
   let product = await Product.findById(req.params.id);
   let newReview = new Review(req.body.review);
   newReview.author = req.user._id;
@@ -13,19 +12,16 @@ module.exports.createReview = async (req, res) => {
   await product.save();
   req.flash("success", "New Review Created!");
 
-  // CHANGED: Redirect to /products
   res.redirect(`/products/${product._id}`);
 };
 
 module.exports.destroyReview = async (req, res) => {
   let { id, reviewId } = req.params;
   
-  // CHANGED: Find Product and pull review
   await Product.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
   await Review.findByIdAndDelete(reviewId);
   
   req.flash("success", "Review Deleted!");
 
-  // CHANGED: Redirect to /products
   res.redirect(`/products/${id}`);
 };
